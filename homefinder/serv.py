f26509a3e42e39f5f4401ec8f047682fce6ef57b
jrz_2022mb <>
2023-11-01 13:54:57 -0400

    recording for 1-nov

--- homefinder/serv.py
+++ homefinder/serv.py
@@ -63,7 +63,7 @@ with open('pgh_homes.json') as f:
 outputKeys = ['HouseID','Neighborhood','Latitude','Longitude','Sale Price','Bathrooms','Bedrooms','Lot Size']
 
 # OLD VERSION OF HOUSES ENDPOINT
-# @app.route('/houses/')
+# @app.route('/houses')
 # def houses():
 #     houses = []
 #     for homeID in db: 
@@ -111,8 +111,86 @@ def houses():
         
     return jsonify( filtered )
 
+# Provide the entire row of data for a house when the client specifies a HouseID
+# Return an error if the HouseID is invalid
+@app.route('/details')
+def details():
+    ident = request.args.get("id", None, type=int)
     
+    if ident is None:
+        response = make_response("No ID")
+        response.status_code = 400 # code 400 is a Bad Request code
+        return response
+    else:
+        if ident not in db:
+            response = make_response("ID not in database")
+            response.status_code = 400
+            return response
+        else:
+            return jsonify( db[ident] )   # wrap this house's data in JSON and send
     
+    # never will reach here
+    return make_response("")
+
+
+# We want to give clients a mechanism to "star" or mark their favorite houses
+# Ideally this will last between multiple sessions (reloads) of the page
+# Here we store everything in memory, which means the starred items will be lost if the server is closed
+# In practice you'd implement this using a mix of user accounts, cookies, and a database
+
+# Return JSON list of starred HouseIDs
+starredItems = set()
+@app.route('/starred')
+def starred():
+    return jsonify( list(starredItems) )
+
+# Star a new HouseID and return a JSON list of starred HouseIDs
+@app.route('/star')
+def star():
+    ident = request.args.get("id", None, type=int)
+    
+    if ident is None:
+        response = make_response("No ID")
+        response.status_code = 400
+        return response
+    else:
+        if ident not in db:
+            response = make_response("ID not in database")
+            response.status_code = 400
+            return response
+        else:
+            if ident in starredItems:
+                return jsonify( list(starredItems) )
+                
+            starredItems.add(ident)
+            return jsonify( list(starredItems) )
+    
+    # never will reach here
+    return make_response("")
+
+# Unstar a HouseID and return a JSON list of starred HouseIDs
+@app.route('/unstar')
+def unstar():
+    ident = request.args.get("id", None, type=int)
+    
+    if ident is None:
+        response = make_response("No ID")
+        response.status_code = 400
+        return response
+    else:
+        if ident not in db:
+            response = make_response("ID not in database")
+            response.status_code = 400
+            return response
+        else:
+            if ident in starredItems:
+                starredItems.remove(ident)
+                
+                return jsonify( list(starredItems) )
+                
+            return jsonify( list(starredItems) )
+
+      
     
     
 # DO NOT DO THIS IN A PRODUCTION ENVIRONMENT
